55385a7d450f97d29414f01714693f875b5c81d5
fcynnek <kchengfung@gmail.com>
2022-12-07 06:08:48 -0800

    Working on comparator

--- src/com/fcynnek/assignment4/DataScienceApplication.java
+++ src/com/fcynnek/assignment4/DataScienceApplication.java
@@ -1,6 +1,8 @@
 package com.fcynnek.assignment4;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.Comparator;
 
 public class DataScienceApplication {
 
@@ -9,8 +11,25 @@ public class DataScienceApplication {
 		String fileName = "master.csv";
 
 		FileService fileService = new FileService();
+		UserPOJO userPOJOs = new UserPOJO();
 		
 		fileService.readStudentData(fileName);
+		
+		String sortedGrades = null;
+
+		UserPOJO[] users = new UserPOJO[FileService.countLines(fileName)];
+
+		Arrays.sort(users, new Comparator<UserPOJO>() {
+
+			@Override
+			public int compare(UserPOJO studentGrade1, UserPOJO studentGrade2) {
+
+				return studentGrade1.getStudentGrade().compareTo(studentGrade2.getStudentGrade());
+			}
+		});
+		for (UserPOJO studentGrade : studentGrades) {
+			System.out.println(sortedGrade);
+		}
 	}
 
 }
--- src/com/fcynnek/assignment4/FileService.java
+++ src/com/fcynnek/assignment4/FileService.java
@@ -4,13 +4,15 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Comparator;
+import java.util.Arrays;
 
-public class FileService {
+public class FileService implements Comparator {
 	// this class will read in the Master List File
 
 	String fileName = "master.csv";
-	
-	public static Integer countLines (String fileName) {
+
+	public static Integer countLines(String fileName) {
 
 		BufferedReader fileReader = null;
 
@@ -38,17 +40,16 @@ public class FileService {
 			}
 		}
 		return numberOfLines;
-	} 
-
+	}
 
 	public UserPOJO[] readStudentData(String fileName) throws IOException {
 
 		BufferedReader fileReader = null;
-		
+
 		Integer numberOfLines = countLines(fileName);
-		
+
 		UserPOJO[] studentDataArray = new UserPOJO[numberOfLines];
-		
+
 		int i = 0;
 
 		try {
@@ -58,32 +59,30 @@ public class FileService {
 
 			try {
 				while ((line = fileReader.readLine()) != null) {
-//					System.out.println(line);
+					// System.out.println(line);
 
 					String[] parsedLine = line.split(",");
 					String parsedStudentID = parsedLine[0];
 					String parsedStudentName = parsedLine[1];
 					String parsedCourseID = parsedLine[2];
 					String parsedStudentGrade = parsedLine[3];
-					
-					
-//					String[] parsedCourseNameAndID = parsedLine[2].split("\\s");
-//					String parsedCourseName = parsedCourseNameAndID[0];
-//					System.out.println(parsedCourseName);
-//					String parsedCourseID = parsedCourseNameAndID[1];
-//					System.out.println(parsedCourseID);
-					
+
+					// String[] parsedCourseNameAndID = parsedLine[2].split("\\s");
+					// String parsedCourseName = parsedCourseNameAndID[0];
+					// System.out.println(parsedCourseName);
+					// String parsedCourseID = parsedCourseNameAndID[1];
+					// System.out.println(parsedCourseID);
 
 					UserPOJO studentData = new UserPOJO();
 					studentData.setStudentID(parsedStudentID);
 					studentData.setStudentName(parsedStudentName);
-//					studentData.setCourseName(parsedCourseName);
+					// studentData.setCourseName(parsedCourseName);
 					studentData.setCourseID(parsedCourseID);
 					studentData.setStudentGrade(parsedStudentGrade);
 
 					studentDataArray[i] = studentData;
 					i = i + 1;
-				}  
+				}
 			} catch (IOException e) {
 				System.out.println("I/O Exception ocurred");
 				e.printStackTrace();
@@ -99,12 +98,31 @@ public class FileService {
 			}
 		}
 		return studentDataArray;
+	
+
+//	public String sortingStudentsGrade() {
+
+//	String sortedGrades = null;
+
+//	UserPOJO[] users = new UserPOJO[countLines(fileName)];
+
+//	Arrays.sort(users, new Comparator<UserPOJO>() {
+
+//		@Override
+//		public int compare(UserPOJO studentGrade1, UserPOJO studentGrade2) {
+//
+//			return studentGrade1.getStudentGrade().compareTo(studentGrade2.getStudentGrade());
+//		}
+//	});
+//	for (UserPOJO user : users) {
+//		System.out.println(user);
+//	}
+//		return sortedGrades;
 	}
-	
-	
-	public String sortingStudents() {
-		
-		
-		return sortingStudents;
+
+	@Override
+	public int compare(Object o1, Object o2) {
+		// TODO Auto-generated method stub
+		return 0;
 	}
 }
